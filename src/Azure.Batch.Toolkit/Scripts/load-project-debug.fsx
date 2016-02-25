b9de2bd6c3177da4d6a7968f5af4461fd2bb74f8
John Azariah <john.azariah@gmail.com>
2016-02-24 17:29:48 -0800

    Introduce helper files for FSX scripting

---
+++ src/Azure.Batch.Toolkit/Scripts/load-project-debug.fsx
@@ -0,0 +1,15 @@
+// Warning: generated file; your changes could be lost when a new file is generated.
+#I __SOURCE_DIRECTORY__
+#load "load-references-debug.fsx"
+#load "../SuccessOrFailure.fs"
+      "../Common.fs"
+      "../JobModel.fs"
+      "../PoolModel.fs"
+      "../Configuration.fs"
+      "../Storage.fs"
+      "../CommonCommands.fs"
+      "../ScriptGenerator.fs"
+      "../Preparation.fs"
+      "../PoolOperations.fs"
+      "../JobModelOperations.fs"
+      "../DSL.fs"
