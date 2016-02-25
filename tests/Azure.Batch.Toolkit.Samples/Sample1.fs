6fb45f7ddbd9dc2a5afe50040c75617a9fb24574
John Azariah <john.azariah@gmail.com>
2016-02-24 17:39:09 -0800

    Add Samples Project

---
+++ tests/Azure.Batch.Toolkit.Samples/Sample1.fs
@@ -0,0 +1,35 @@
+﻿namespace Azure.Batch.Toolkit.Samples
+
+module Sample1 =
+    open Batch.Toolkit
+    open Batch.Toolkit.DSL
+
+    let helloUserCommand = 
+        ``:parametric_cmd`` "echo 'Hello %user%'" ``:with_params`` ["user"]
+    
+    let workloadUnitTemplate = 
+        ``:unit`` 
+            ``:admin`` false 
+            ``:main`` 
+                [
+                    ``:do`` helloUserCommand
+                ] 
+            ``:finally``
+                []
+            ``:files`` 
+                []
+
+    let workload =
+        ``:workload``
+            ``:units`` 
+                [
+                    workloadUnitTemplate 
+                ]
+            ``:files`` 
+                []
+            ``:arguments`` 
+                [
+                    ``:range`` "user" ``:over`` ["John"; "Ivan"; "Pablo"]
+                ]
+
+    workload |> runSampleWorkload
---
+++ tests/Azure.Batch.Toolkit.Samples/SampleCommon.fs
@@ -0,0 +1,19 @@
+﻿namespace Azure.Batch.Toolkit.Samples
+
+[<AutoOpen>]
+module SampleCommon =
+    open Batch.Toolkit
+    open Batch.Toolkit.DSL
+    open Batch.Toolkit.PoolOperations
+    open Batch.Toolkit.WorkloadOperations
+
+    let runSampleWorkload workload = 
+        let pool = NamedPool { NamedPoolName = PoolName "sample-pool"; NamedPoolSpecification = GetDefaultPoolSpecification }        
+        let (batchConfig, storageConfig) = 
+            succeed {        
+                let! batchConfig = readConfig<BatchConfiguration>("batch-config.json")
+                let! storageConfig = readConfig<StorageConfiguration>("storage-config.json")
+                return (batchConfig, storageConfig)
+            } |> getOrThrow
+
+        workload |> RunWorkloadOnPool batchConfig storageConfig pool
